3260cca41dea6d6e709c73a71eab0fa785800719
Valentin271 <valentin.d77@free.fr>
2023-06-04 12:28:31 +0200

    feat: token validation in auth request guard

--- src/auth.rs
+++ src/auth.rs
@@ -5,6 +5,12 @@ use rocket::{
     Request,
 };
 
+use crate::database::Db;
+
+/// Number of hours the auth token is valid
+#[cfg(not(debug_assertions))]
+const TOKEN_VALIDITY: u8 = 24;
+
 /// Authentification request guard.
 /// Must be a parameter of authenticated routes.
 ///
@@ -12,13 +18,41 @@ use rocket::{
 pub struct Auth;
 
 #[cfg(debug_assertions)]
-fn is_valid(_: &Request<'_>) -> bool {
+async fn is_valid(_req: &Request<'_>, _db: Db) -> bool {
     true
 }
 
 #[cfg(not(debug_assertions))]
-fn is_valid(req: &Request<'_>) -> bool {
-    todo!()
+async fn is_valid(req: &Request<'_>, db: Db) -> bool {
+    use std::ops::Sub;
+
+    use chrono::Duration;
+    use diesel::prelude::*;
+    use provoit_types::{models::users::User, schema::users};
+
+    let cookie = req.cookies().get("token");
+
+    if let Some(cookie) = cookie {
+        let token = cookie.value().to_owned();
+        let now_minus_validity = chrono::Local::now()
+            .naive_local()
+            .sub(Duration::hours(TOKEN_VALIDITY as i64));
+
+        let user: Result<Option<User>, diesel::result::Error> = db
+            .run(move |conn| {
+                users::table
+                    .select(users::all_columns)
+                    .filter(users::token.eq(token))
+                    .filter(users::token_gentime.gt(now_minus_validity))
+                    .first(conn)
+                    .optional()
+            })
+            .await;
+
+        user.is_ok() && user.unwrap().is_some()
+    } else {
+        false
+    }
 }
 
 #[rocket::async_trait]
@@ -26,7 +60,9 @@ impl<'r> FromRequest<'r> for Auth {
     type Error = ();
 
     async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        if is_valid(req) {
+        let db = Db::from_request(req).await.unwrap();
+
+        if is_valid(req, db).await {
             Outcome::Success(Auth)
         } else {
             Outcome::Failure((Status::Unauthorized, ()))
