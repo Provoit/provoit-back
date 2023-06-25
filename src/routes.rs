76a410edd89f77bef78cd494dc2829202ca25bd1
Valentin271 <valentin.d77@free.fr>
2023-06-25 16:29:45 +0200

    style: format

--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,11 @@
 use rocket::response::Debug;
 
 pub mod auth;
+pub mod timings;
 pub mod trips;
 pub mod users;
 pub mod vehicles;
 pub mod version;
-pub mod timings;
 
 /// Result for a route using diesel
 type DbResult<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
